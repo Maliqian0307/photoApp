6fa543d97857b2161e148b298bc73b0a0cdf4416
Liqian Ma <liqianma2023@u.northwestern.edu>
2022-03-18 18:27:53 -0500

    change api

--- hints/hint-1/NavBar.js
+++ hints/hint-1/NavBar.js
@@ -18,7 +18,7 @@ class NavBar extends React.Component {
             <nav className="main-nav">
                 <h1>{this.props.title}</h1>
                 <ul>   
-                    <li><a href="/api">API Docs</a></li>
+                    <li><a href="https://photo-app-secured.herokuapp.com/api">API Docs</a></li>
                     <li><span>username</span></li>
                     <li><a href="/logout">Sign out</a></li>
                 </ul> 
